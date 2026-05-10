5d1df7737089e141184cf4f57509be3b6f648ee8
fenrir-mack <10436455@mackenzista.com.br>
2026-05-10 14:53:26 -0300

    arrumando login e dashboard

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 @app.route("/login", methods=["GET", "POST"])
 def login():
 
@@ -12,14 +12,23 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/cadastro")
+@app.route("/cadastro", methods=["GET", "POST"])
 def cadastro():
+    if request.method == "POST":
+        return redirect(url_for("login"))
     return render_template("cadastro.html")
 
 
-@app.route("/esqueceu-senha")
+@app.route("/esqueceu-senha", methods=["GET", "POST"])
 def esqueceu_senha():
+    if request.method == "POST":
+        return redirect(url_for("confirmacao_senha"))
     return render_template("esqueceu_senha.html")
+
+
+@app.route("/confirmacao-senha")
+def confirmacao_senha():
+    return render_template("confirmacao_senha.html")
 
 
 @app.route("/dashboard")
